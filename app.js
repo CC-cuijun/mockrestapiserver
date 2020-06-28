17daabde5bfbc6eaea1b77bb3d3c871635ce6116
CC <51074375+312526353@users.noreply.github.com>
2020-06-28 20:19:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 app.use(bodyParser.json({limit: '1mb'}));  //指定参数使用 json 格式
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+// 模拟直接返回数据，并替换返回中的参数
 app.post('/listBindBrands', function (req, res) {
 /*    fs.readFile( __dirname + "/resdata/" + "brandlist.json", 'utf8', function (err, data) {
         console.log( data );
@@ -24,21 +24,21 @@ app.post('/listBindBrands', function (req, res) {
                 "result": false,
                 "code": 200,
                 "msg": "null",
-                "data": null
+                "data": req.body.brandcode
             }
         },
         "data": null
     }
     res.send( data );
 })
-
+//模拟get请求
 app.get('/getsyncstatus', function (req, res) {
     fs.readFile( __dirname + "/resdata/" + "syncstatusres.json", 'utf8', function (err, data) {
         console.log( data );
         res.send( data );
     });
 })
-
+//模拟get请求并带参数
 app.get('/getbrandlist/:id', function (req, res) {
     fs.readFile( __dirname + "/resdata/" + "brandlist.json", 'utf8', function (err, data) {
         data = JSON.parse( data );
@@ -47,7 +47,7 @@ app.get('/getbrandlist/:id', function (req, res) {
         res.send( JSON.stringify(brand));
     });
 })
-
+//模拟post请求
 app.post('/updateOrganization', function (req, res) {
     console.log(req.body)
     fs.readFile( __dirname + "/resdata/" + "updatestatusres.json", 'utf8', function (err, data) {
@@ -55,7 +55,8 @@ app.post('/updateOrganization', function (req, res) {
         res.send( data );
     });
 });
-
+//模拟put请求
+//模拟delete请求
 
 const PORT = process.env.PORT || 8001;
 server.listen(PORT);
